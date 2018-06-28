94adffbfaefed7bf596bff653f4c493178bc9eb0
Rafael Marchesini Macario <T709768@santander.com.br>
2018-06-28 13:35:25 -0300

    Commit inicial

---
+++ spring-boot-ssl-authentication-client/src/main/java/br/com/rmacario/springbootsslauthenticationclient/SpringBootSslAuthenticationClientApplication.java
@@ -0,0 +1,12 @@
+package br.com.rmacario.springbootsslauthenticationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSslAuthenticationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSslAuthenticationClientApplication.class, args);
+	}
+}
